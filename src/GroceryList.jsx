aba3ea1520d74bcffae9a20a022c70e3e7551909
Austin <austinglarson@gmail.com>
2024-08-16 19:14:57 -0700

    Added the grocery list inputs.

--- src/GroceryList.jsx
+++ src/GroceryList.jsx
@@ -4,14 +4,9 @@ import React from 'react'
 let nextId = 0;
 
 export default function GroceryList() {
-    const [groceryListItem, setGroceryListItem] = React.useState('');
     const [groceryList, setGroceryList] = React.useState(() =>
-        JSON.parse(localStorage.getItem('groceryList')) || [{id: nextId++, checked: false, text: ''}]
+        JSON.parse(localStorage.getItem('groceryList')) || [{id: nextId++, checked: false, delete: false, text: '',}]
     )
-
-    function updateGroceryListItem(event) {
-        setGroceryListItem(event.target.value)
-    }
 
     React.useEffect(() => {
         console.log(groceryList)
@@ -19,20 +14,33 @@ export default function GroceryList() {
     }, [groceryList])
 
     function updateGroceryList(event, id) {
-        console.log(nextId)
+        //console.log(nextId)
+        const itemValue = event.target.value;
+
         if (event.key === 'Enter') {
             console.log('enter')
-            setGroceryList(prevGroceryList => [...prevGroceryList, {id: nextId++, checked: false, text: event.target.value}])
-        } else {
-            setGroceryList(prevGroceryList => prevGroceryList.map(prevItem => {
-                if (prevItem.id === id) {
-                    console.log('test1')
-                    return {...prevItem, text: event.target.value}
-                } else {
-                    console.log('test3')
-                    return prevItem
+            setGroceryList(prevGroceryList => [...prevGroceryList, {id: nextId++, checked: false, delete: false, text: itemValue}])
+        } else if (itemValue === '' && event.key === 'Backspace') {
+            setGroceryList(prevGroceryList => {
+                const newGroceryList = []
+                for (let item of prevGroceryList) {
+                    if (item.id === id && item.delete) {
+                        newGroceryList.filter((item) => item.id !== id)
+                    } else if (item.id === id) {
+                        item.delete = true
+                        newGroceryList.push(item)
+                    } else {
+                        newGroceryList.push(item)
+                    }
                 }
-            }))
+                return newGroceryList
+            })
+        } else {
+            setGroceryList(prevGroceryList => prevGroceryList.map(prevItem =>
+                prevItem.id === id 
+                ? {...prevItem, delete: false, text: itemValue}
+                : prevItem
+            ))
         }
     }
 
@@ -40,17 +48,27 @@ export default function GroceryList() {
         setGroceryList(prevGroceryList => [...prevGroceryList, {id: nextId++, checked: false, text: event.target.value}])
     }
 
+    function toggleGroceryListItemCheck(event, id) {
+        console.log(event.target.checked)
+        setGroceryList(prevGroceryList => prevGroceryList.map(prevItem =>
+            prevItem.id === id 
+            ? {...prevItem, checked: event.target.checked}
+            : prevItem
+        ))
+    }
+
     /* const groceryListItems = groceryList.map((item) =>
         <GroceryListItem key={item.id} {...item} onChange={updateGroceryList} />
     ) */
 
     return (
-        <div>
+        <div className='grocery-list'>
             {groceryList.map(item => (
-                <div key={item.id}>
-                    <input type="checkbox" />
+                <div key={item.id} className={item.checked ? 'checked' : ''}>
+                    <input type="checkbox" checked={item.checked} onChange={(event) => toggleGroceryListItemCheck(event, item.id)} />
                     <input type="text"
                         onKeyUp={(event) => updateGroceryList(event, item.id)}
+                        autoFocus
                         /* onChange={(event) => updateGroceryList(event, item.id)} */
                         /* onKeyDown={(event) => event.key === 'Enter' && event.preventDefault()} */ />
                 </div>
